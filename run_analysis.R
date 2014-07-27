e6dfeaae3c279d7e8956dbd2c4bfa88570ee38e0
Peter Kurpinski <pariscatblue88@gmail.com>
2014-07-27 23:59:50 +0100

    almost last version, last corrections and gravity component to add in the Monday morning, goodnight!

--- run_analysis.R
+++ run_analysis.R
@@ -1,15 +1,15 @@
-## 0: installations, environment etc.
-if(!"data.table" %in% installed.packages()) install.packages("data.table")
+## installations, environment etc.
+if(!"plyr" %in% installed.packages()) install.packages("plyr")
 
-#1. downloading and unzipping data set
+
+#0. ----- downloading and un-zipping data set
 if(!file.exists("./data/UCI HAR Dataset/README.txt")){
   fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
   download.file(fileUrl, destfile="./data/Dataset.zip", method="auto")
   unzip("./data/Dataset.zip",exdir="./data")
 }
 
-#1. loading and corecting feautures columns' names
-
+#1. ----- loading features names to modify accordding to the CodeBook 
 colNames <- read.table("./data/UCI HAR Dataset/features.txt",colClasses = "character")[,2]
 
 addXYZ <- function(name){
@@ -32,10 +32,11 @@ addMeanFreqXYZ <- function(name){
 addFreq <- function(name) paste(name,".MeanFreq",sep = "")
 
 colScrap = vector(mode="integer")
-colScrap <- 1:6 
+colScrap <- 1:6
+# ----- time domain variables
 colNames[1:6] <- addXYZ("tBodyAcc")
-colScrap <- append(colScrap, 41:46)
-colNames[41:46] <- addXYZ("tGravityAcc")
+# colScrap <- append(colScrap, 41:46)
+# colNames[41:46] <- addXYZ("tGravityAcc")
 colScrap <- append(colScrap,  81:86)
 colNames[81:86] <- addXYZ("tBodyAccJerk")
 colScrap <- append(colScrap,  121:126)
@@ -43,16 +44,17 @@ colNames[121:126] <- addXYZ("tBodyGyro")
 colScrap <- append(colScrap,  161:166)
 colNames[161:166] <- addXYZ("tBodyGyroJerk")
 colScrap <- append(colScrap,  201:202)
-colNames[201:202] <- addSdMn("tBodyAccMag")
-colScrap <- append(colScrap,  214:215)
-colNames[214:215] <- addSdMn("tGravityAccMag")
+colNames[201:202] <- addSdMn("tBodyAcc.Mag")
+# colScrap <- append(colScrap,  214:215)
+# colNames[214:215] <- addSdMn("tGravityAcc.Mag")
 colScrap <- append(colScrap,  227:228)
-colNames[227:228] <- addSdMn("tBodyAccJerkMag")
+colNames[227:228] <- addSdMn("tBodyAccJerk.Mag")
 colScrap <- append(colScrap,  240:241)
-colNames[240:241] <- addSdMn("tBodyGyroMag")
+colNames[240:241] <- addSdMn("tBodyGyro.Mag")
 colScrap <- append(colScrap,  253:254)
-colNames[253:254] <- addSdMn("tBodyGyroJerkMag")
+colNames[253:254] <- addSdMn("tBodyGyroJerk.Mag")
 colScrap <- append(colScrap,  266:271)
+# ----- frequency  domain variables
 colNames[266:271] <- addXYZ("fBodyAcc")
 colScrap <- append(colScrap,  345:350)
 colNames[345:350] <- addXYZ("fBodyAccJerk")
@@ -63,61 +65,70 @@ colNames[424:429] <- addXYZ("fBodyGyro")
 colScrap <- append(colScrap,  452:454)
 colNames[452:454] <- addMeanFreqXYZ("fBodyGyro")
 colScrap <- append(colScrap,  503:504)
-colNames[503:504] <- addSdMn("fBodyAccMag")
+colNames[503:504] <- addSdMn("fBodyAcc.Mag")
 colScrap <- append(colScrap,  516:517)
-colNames[516:517] <- addSdMn("fBodyBodyAccJerkMag")
+colNames[516:517] <- addSdMn("fBodyBodyAccJerk.Mag")
 colScrap <- append(colScrap,  529:530)
-colNames[529:530] <- addSdMn("fBodyBodyGyroMag")
+colNames[529:530] <- addSdMn("fBodyBodyGyro.Mag")
 colScrap <- append(colScrap,  539)
-colNames[539] <- addFreq("fBodyBodyGyroMag")
-colScrap <- append(colScrap,  542:543)
-colNames[542:543] <- addSdMn("fBodyBodyGyroJerkMag")
-colScrap <- append(colScrap,  552)
-colNames[552] <- addFreq("fBodyBodyGyroJerkMag")
+colNames[539] <- addFreq("fBodyBodyGyro.Mag")
+# colScrap <- append(colScrap,  542:543)
+# colNames[542:543] <- addSdMn("fBodyBodyGyroJerk.Mag")
+# colScrap <- append(colScrap,  552)
+# colNames[552] <- addFreq("fBodyBodyGyroJerk.Mag")
+# ----- angular variables
 colScrap <- append(colScrap,  555:561)
-colNames[555] <- "angle.Gravity.tBodyAccMean"
-colNames[556] <- "angle.GravityMean.tBodyAccJerkMean"
-colNames[557] <- "angle.Gravity.tBodyGyroMean"
-colNames[558] <- "angle.GravityMean.tBodyGyroJerkMean"
-colNames[559] <- "angle.GravityMean.X"
-colNames[560] <- "angle.GravityMean.Y"
-colNames[561] <- "angle.GravityMean.Z"
+colNames[555] <- "angleBetw.Gravity.tBodyAccMean"
+colNames[556] <- "angleBetw.GravityMean.tBodyAccJerkMean"
+colNames[557] <- "angleBetw.Gravity.tBodyGyroMean"
+colNames[558] <- "angleBetw.GravityMean.tBodyGyroJerkMean"
+# ----- position of the recording device ref to the gravity vector
+colNames[559] <- "angleBetw.GravityMean.X"
+colNames[560] <- "angleBetw.GravityMean.Y"
+colNames[561] <- "angleBetw.GravityMean.Z"
 
 #colNames
 
-#1B. loading the training and the test set into R  
+#1B. ----- loading the train and the test sets and extracting mean/sd columns  
 
 train <- read.table("./data/UCI HAR Dataset/train/X_train.txt",
-                    colClasses = "numeric", col.names=colNames)#, fill = T)#[,colScrap]
+                    colClasses = "numeric", col.names=colNames)
 train <- subset(train, select = colScrap)
 
 test <- read.table("./data/UCI HAR Dataset/test/X_test.txt",
-                   colClasses = "numeric", col.names=colNames)#, fill = T)#[,colScrap]
+                   colClasses = "numeric", col.names=colNames)
 test <- subset(test, select = colScrap)
 
-#1C. loading activities labels and merging with train as a activity column
-actLabels <- read.table("./data/UCI HAR Dataset/activity_labels.txt", stringsAsFactors=T,
-                        fill = T)
-actColTrain <- merge(actLabels, read.table("./data/UCI HAR Dataset/train/y_train.txt", colClasses="numeric", 
-                                           fill=T), by.x="V1", by.y="V1", all=T )[,2]
 
-actColTest <- merge(actLabels, read.table("./data/UCI HAR Dataset/test/y_test.txt", colClasses="numeric", 
-                                          fill=T), by.x="V1", by.y="V1", all=T )[,2]
+#1C. ----- loading activities' labels
+actLabels <- read.table("./data/UCI HAR Dataset/activity_labels.txt", 
+                        stringsAsFactors=T)#fill = T)
+names(actLabels) = c("idActiv","activity")
 
-#1D. merging activity labels with test as an activity column (the last)
-train <- cbind(train, activity=actColTrain)
-test <- cbind(test, activity=actColTest)
+#1D. ----- loading and merging activity columns with test and train
+trainActCol <- read.table("./data/UCI HAR Dataset/train/y_train.txt", colClasses="numeric")[,1]
+testActCol <- read.table("./data/UCI HAR Dataset/test/y_test.txt", colClasses="numeric")[,1]
+train <- cbind(idActiv=trainActCol, train)
+test <- cbind(idActiv=testActCol, test)
 
-#1E. loading and adding subjects' ids as the first column to train and test collections
+#1E. ----- loading and adding subjects' ids as the first column to train and test
+train <- cbind(subject=read.table("./data/UCI HAR Dataset/train/subject_train.txt", 
+                                  colClasses="integer")[,1], train)
+test <- cbind(subject=read.table("./data/UCI HAR Dataset/test/subject_test.txt", 
+                                 colClasses="integer")[,1], test)
 
-train <- cbind(subject=read.table("./data/UCI HAR Dataset/train/subject_train.txt", colClasses="factor", 
-                                  fill=T)$V1, train)
-test <- cbind(subject=read.table("./data/UCI HAR Dataset/test/subject_test.txt", colClasses="factor", 
-                                 fill=T)$V1, test)
-#1F. a new tidy dataframe dt
-
-# trn$testOrTrain <- "train"
-# tst$testOrTrain <- "test"
+#2. ----- merging test and train data into a new dataframe
 dt <- rbind(train,test)
+train = NULL
+test = NULL
 
-write.csv(dt, file = "./data/tidy.csv.txt", row.names = FALSE)
+#5A. -----  taking average of each variable for each combination of subject and activity
+tidy <- aggregate(subset(dt, select=c(-subject,-idActiv)), 
+                  by=list(dt$subject,dt$idActiv), FUN = mean)
+names(tidy)[1:2] <- c("subject","idActiv")
+tidy <- merge(x=actLabels, y=tidy, by="idActiv", all=T )
+tidy$idActiv <- NULL
+
+#5B. ----- saving the data frame of this as a set of tidy data
+write.table(tidy, file = "./data/myTidyDataSet.txt", sep = "\t", row.names = FALSE)
+
